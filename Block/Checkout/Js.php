e18c46cd6f7252a511573d2402351a6e21db86de
siemsonNarson <adam.from.fishbane@gmail.com>
2020-09-23 15:41:51 +0200

    updated with js code extended

--- Block/Checkout/Js.php
+++ Block/Checkout/Js.php
@@ -1,20 +1,44 @@
 <?php
 namespace Yotpo\Loyalty\Block\Checkout;
 
-use Magento\Framework\App\Config\ScopeConfigInterface;
-
+/**
+ * Class Js
+ * @package Yotpo\Loyalty\Block\Checkout
+ */
 class Js extends \Magento\Framework\View\Element\Template
 {
-    public function __construct(ScopeConfigInterface $scopeConfig)
+
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $_scopeConfig;
+
+    /**
+     * Js constructor.
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        array $data = [])
     {
+        parent::__construct($context, $data);
         $this->_scopeConfig = $scopeConfig;
     }
 
+    /**
+     * @return mixed
+     */
     public function getGuid()
     {
         return $this->_scopeConfig->getValue("yotpo_loyalty/general_settings/swell_guid");
     }
 
+    /**
+     * @return mixed
+     */
     public function getInstanceId()
     {
         return $this->_scopeConfig->getValue("yotpo_loyalty/sync_advanced/swell_instance_id");
--- view/frontend/web/js/view/summary/custom.js
+++ view/frontend/web/js/view/summary/custom.js
@@ -9,12 +9,14 @@ define(
                 template: 'Yotpo_Loyalty/summary/custom'
             },
             loadJsCustomAfterKoRender: function() {
-                var adminSettings = window.valuesConfig;
-                console.log(adminSettings);
+                var guidId = window.valuesConfig;
+                var instanceId = window.swellInstanceId;
+                var url = 'https://cdn-widgetsrepository.yotpo.com/v1/loader/' + guidId;
                 var script = document.createElement('script');
-                script.src = 'https://cdn-widgetsrepository.yotpo.com/v1/loader/X-GUID-X'
+                script.src = url
                 script.setAttribute('src_type', 'url')
                 document.head.appendChild(script)
+                jQuery('.yotpo-widget-instance').attr('data-yotpo-instance-id', instanceId);
             }
         });
     }
