775462485750669280648083c4a5325ac32f370f
pini-girit <pini@girit.biz>
2020-09-27 12:41:31 +0300

    v1.1.9: Added delete_rule API endpoint and added support for deleting multiple coupons or ruled on the same request

--- Model/Api/Swell/Index/DeleteCouponManagement.php
+++ Model/Api/Swell/Index/DeleteCouponManagement.php
@@ -22,16 +22,24 @@ class DeleteCouponManagement extends AbstractSwell implements \Yotpo\Loyalty\Api
     protected $_couponFactory;
 
     /**
+     * @var \Magento\SalesRule\Model\RuleFactory
+     */
+    protected $_ruleFactory;
+
+    /**
      * @param \Yotpo\Loyalty\Helper\Data $yotpoHelper
      * @param \Yotpo\Loyalty\Helper\Schema $yotpoSchemaHelper
      * @param \Magento\SalesRule\Model\CouponFactory $couponFactory
+     * @param \Magento\SalesRule\Model\RuleFactory $couponFactory
      */
     public function __construct(
         \Yotpo\Loyalty\Helper\Data $yotpoHelper,
         \Yotpo\Loyalty\Helper\Schema $yotpoSchemaHelper,
-        \Magento\SalesRule\Model\CouponFactory $couponFactory
+        \Magento\SalesRule\Model\CouponFactory $couponFactory,
+        \Magento\SalesRule\Model\RuleFactory $ruleFactory
     ) {
         $this->_couponFactory = $couponFactory;
+        $this->_ruleFactory = $ruleFactory;
         parent::__construct($yotpoHelper, $yotpoSchemaHelper);
     }
 
@@ -47,33 +55,69 @@ class DeleteCouponManagement extends AbstractSwell implements \Yotpo\Loyalty\Api
             ]);
         }
         try {
+            $response = [
+                "success" => true,
+                "message" => "Success",
+                "data" => [],
+            ];
+
             //Extract Request Params:
-            $couponId = $this->_yotpoHelper->getRequest()->getParam('id');
+            $couponIds = $this->_yotpoHelper->getRequest()->getParam('id');
+            if (!(is_array($couponIds) || is_object($couponIds))) {
+                $couponIds = explode(",", $couponIds);
+            }
+            $couponIds = array_filter(array_map('trim', (array) $couponIds));
+
+            $deleteRule = (bool) $this->_yotpoHelper->getRequest()->getParam('delete_rule');
             //===========================================================================================//
 
-            if (!$couponId) {
+            if (!$couponIds) {
                 return $this->_yotpoHelper->jsonEncode([
                     "error" => "`id` is a required field!"
                 ]);
             }
 
-            $coupon = $this->_couponFactory->create()->load($couponId);
-            if ($coupon->getId() && $coupon->getRuleId()) {
-                $coupon->delete();
-            } else {
-                return $this->_yotpoHelper->jsonEncode([
-                    "success" => true,
-                    "message" => 'There is no coupon with this id'
-                ]);
+            foreach ($couponIds as $couponId) {
+                try {
+                    $ruleId = $coupon->getRuleId();
+                    $coupon = $this->_couponFactory->create()->load($couponId);
+                    if ($coupon->getId() && $coupon->getRuleId()) {
+                        $coupon->delete();
+                        $response["data"][$couponId] = [
+                            "success" => true
+                        ];
+                    } else {
+                        $response["data"][$couponId] = [
+                            "success" => true,
+                            "message" => 'There is no coupon with this id'
+                        ];
+                    }
+                    $response["data"][$couponId]["rule_deleted"] = false;
+                    if ($ruleId && $deleteRule) {
+                        $rule = $this->_couponFactory->create()->load($ruleId);
+                        if ($rule->getId() && $rule->getRuleId()) {
+                            $rule->delete();
+                            $response["data"][$couponId]["rule_deleted"] = true;
+                        }
+                    }
+                } catch (\Exception $e) {
+                    $this->_yotpoHelper->log("[Yotpo Loyalty API - DeleteCoupon - ERROR] " . $e->getMessage() . "\n" . $e->getTraceAsString(), "error");
+                    $response["data"][$couponId] = [
+                        "error" => 'An error has occurred while trying to delete coupon ID' . $couponId
+                    ];
+                    $response["success"] = false;
+                    $response["error"] = true;
+                    $response["message"] = "Has errors";
+                }
             }
         } catch (\Exception $e) {
             $this->_yotpoHelper->log("[Yotpo Loyalty API - DeleteCoupon - ERROR] " . $e->getMessage() . "\n" . $e->getTraceAsString(), "error");
             return $this->_yotpoHelper->jsonEncode([
-                "error" => 'An error has occurred while trying to delete coupon ID' . $couponId
+                "error" => 'An error has occurred while trying to delete coupon(s)' . $couponId,
+                "data" => $couponIds
             ]);
         }
-        return $this->_yotpoHelper->jsonEncode([
-            "success" => true
-        ]);
+
+        return $this->_yotpoHelper->jsonEncode($response);
     }
 }
---
+++ Model/Api/Swell/Index/DeleteRuleManagement.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Yotpo\Loyalty\Model\Api\Swell\Index;
+
+use Yotpo\Loyalty\Model\Api\Swell\AbstractSwell;
+
+class DeleteRuleManagement extends AbstractSwell implements \Yotpo\Loyalty\Api\Swell\Index\DeleteRuleManagementInterface
+{
+    /**
+     * @var \Yotpo\Loyalty\Helper\Data
+     */
+    protected $_yotpoHelper;
+
+    /**
+     * @var \Yotpo\Loyalty\Helper\Schema
+     */
+    protected $_yotpoSchemaHelper;
+
+    /**
+     * @var \Magento\SalesRule\Model\RuleFactory
+     */
+    protected $_ruleFactory;
+
+    /**
+     * @param \Yotpo\Loyalty\Helper\Data $yotpoHelper
+     * @param \Yotpo\Loyalty\Helper\Schema $yotpoSchemaHelper
+     * @param \Magento\SalesRule\Model\RuleFactory $ruleFactory
+     */
+    public function __construct(
+        \Yotpo\Loyalty\Helper\Data $yotpoHelper,
+        \Yotpo\Loyalty\Helper\Schema $yotpoSchemaHelper,
+        \Magento\SalesRule\Model\RuleFactory $ruleFactory
+    ) {
+        $this->_ruleFactory = $ruleFactory;
+        parent::__construct($yotpoHelper, $yotpoSchemaHelper);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function postDeleteRule()
+    {
+        if (!$this->isAuthorized()) {
+            return $this->_yotpoHelper->jsonEncode([
+                "error" => 1,
+                "message" => "Access Denied!"
+            ]);
+        }
+        try {
+            $response = [
+                "success" => true,
+                "message" => "Success",
+                "data" => [],
+            ];
+
+            //Extract Request Params:
+            $ruleIds = $this->_yotpoHelper->getRequest()->getParam('id');
+            if (!(is_array($ruleIds) || is_object($ruleIds))) {
+                $ruleIds = explode(",", $ruleIds);
+            }
+            $ruleIds = array_filter(array_map('trim', (array) $ruleIds));
+            //===========================================================================================//
+
+            if (!$ruleIds) {
+                return $this->_yotpoHelper->jsonEncode([
+                    "error" => "`id` is a required field!"
+                ]);
+            }
+
+            foreach ($ruleIds as $ruleId) {
+                try {
+                    $rule = $this->_ruleFactory->create()->load($ruleId);
+                    if ($rule->getId() && $rule->getRuleId()) {
+                        $rule->delete();
+                        $response["data"][$ruleId] = [
+                            "success" => true
+                        ];
+                    } else {
+                        $response["data"][$ruleId] = [
+                            "success" => true,
+                            "message" => 'There is no rule with this id'
+                        ];
+                    }
+                } catch (\Exception $e) {
+                    $this->_yotpoHelper->log("[Yotpo Loyalty API - DeleteRule - ERROR] " . $e->getMessage() . "\n" . $e->getTraceAsString(), "error");
+                    $response["data"][$ruleId] = [
+                        "error" => 'An error has occurred while trying to delete rule ID' . $ruleId
+                    ];
+                    $response["success"] = false;
+                    $response["error"] = true;
+                    $response["message"] = "Has errors";
+                }
+            }
+        } catch (\Exception $e) {
+            $this->_yotpoHelper->log("[Yotpo Loyalty API - DeleteRule - ERROR] " . $e->getMessage() . "\n" . $e->getTraceAsString(), "error");
+            return $this->_yotpoHelper->jsonEncode([
+                "error" => 'An error has occurred while trying to delete rule(s)' . $ruleId,
+                "data" => $ruleIds
+            ]);
+        }
+
+        return $this->_yotpoHelper->jsonEncode($response);
+    }
+}
