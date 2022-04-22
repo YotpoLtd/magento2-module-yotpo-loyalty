062f69509bdd7ba48f4a71520dec308a77dff468
bdn <bohdan.bakalov@eltrino.com>
2022-04-23 02:50:01 +0300

    API url generation for the snippet endpoint 'rest/V1/swell/session/snippet' moved to block

--- Block/Snippet.php
+++ Block/Snippet.php
@@ -4,4 +4,25 @@ namespace Yotpo\Loyalty\Block;
 
 class Snippet extends \Yotpo\Loyalty\Block\AbstractBlock
 {
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    public $storeManager;
+
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Yotpo\Loyalty\Helper\Data $yotpoHelper,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+    ) {
+        parent::__construct($context, $yotpoHelper);
+        $this->storeManager = $storeManager;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSwellSessionSnippetApiUrl()
+    {
+        return $this->storeManager->getStore()->getUrl('rest/V1/swell/session/snippet');
+    }
 }
